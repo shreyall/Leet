ac27e9732c6a2abaa56698192352c3d5e5d3d6b6
Shreyal Shah <shreyalshah@live.com>
2020-01-03 16:22:27 -0500

    added two pointer solution for kDiffPairs ( 532 )

--- Leetcode/Arrays/kDiffPairs.js
+++ Leetcode/Arrays/kDiffPairs.js
@@ -1,5 +1,6 @@
 // LEETCODE EASY ( 532 )
 
+// Using hashmap, O(n) time and O(n) space
 const findPairs = (nums, k) => {
   if (k < 0) return 0;
   let m1 = new Map();
@@ -16,5 +17,26 @@ const findPairs = (nums, k) => {
     }
   });
 
+  return count;
+};
+
+// Using two pointer, O(nlogn) time and O(1) space
+
+const findPairs2P = (nums, k) => {
+  if (k < 0 || nums.length === 0) return 0;
+  nums.sort((a, b) => {
+    return a - b;
+  });
+  let left = 0,
+    right = 0,
+    count = 0;
+  while (right < nums.length) {
+    if (nums[right] - nums[left] > k) left++;
+    else if (nums[right] - nums[left] < k || left === right) right++;
+    else {
+      count++, right++, left++;
+      while (right < nums.length && nums[right] === nums[right - 1]) right++;
+    }
+  }
   return count;
 };
