4e578730774c80b4d64f6460a782ede3b9efb763
Shreyal Shah <shreyalshah@live.com>
2020-01-09 18:30:31 -0500

    added levelOrderBinaryTree ( 102 )

---
+++ Leetcode/Trees/levelOrderBinaryTree.js
@@ -0,0 +1,22 @@
+// LEETCODE MEDIUM ( 102 )
+
+//level order traversal (breadth first search) of a binary tree
+
+const levelOrder = root => {
+  if (root === null) return [];
+  let results = [];
+  let q = [root];
+
+  while (q.length > 0) {
+    let level = [],
+      len = q.length;
+    for (let i = 0; i < len; i++) {
+      curr = q.shift();
+      if (curr.left) q.push(curr.left);
+      if (curr.right) q.push(curr.right);
+      level.push(curr.val);
+    }
+    results.push(level);
+  }
+  return results;
+};
