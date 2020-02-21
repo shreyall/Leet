8f3566535630382c5a9be7c98b4e0e957472aa2a
Shreyal Shah <shreyalshah@live.com>
2020-02-21 15:57:36 -0500

    updated bestTimeToBuyandSellStock

--- Leetcode/Arrays/bestTimeToBuy&SellStock.js
+++ Leetcode/Arrays/bestTimeToBuy&SellStock.js
@@ -13,3 +13,25 @@ const maxProfit = prices => {
   }
   return profit;
 };
+
+// dynamic programming solution  ... idea is to store the minimum amount to buy at,
+//so you can calculate profit with respect to it
+
+const maxProfit2 = prices => {
+  let maxProfit = Number.NEGATIVE_INFINITY;
+  let minPrice = prices[0];
+
+  for (var i of prices) {
+    let num = i - minPrice;
+    if (num > maxProfit) {
+      maxProfit = num;
+    }
+    if (i < minPrice) {
+      minPrice = i;
+    }
+  }
+};
+
+var arr = [7, 1, 5, 3, 6, 4];
+
+console.log(maxProfit2(arr));
