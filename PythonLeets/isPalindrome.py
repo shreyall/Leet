c884ee2b30f9a1efdcf51798aff087918f755542
Shreyal Shah <shreyalshah@live.com>
2020-07-03 01:33:36 -0400

    valid palindrome -- leet 125

---
+++ PythonLeets/isPalindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s)-1
+
+        while(i < j):
+            if(s[i].isalnum() and s[j].isalnum()):
+                if(s[i].lower() != s[j].lower()):
+                    return False
+                else:
+                    i += 1
+                    j -= 1
+            elif not s[i].isalnum():
+                i += 1
+            else:
+                j -= 1
+        return True
